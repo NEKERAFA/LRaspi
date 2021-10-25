e32d2a64f8f68f427310d9576e87cc7ac0686f09
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2021-10-25 18:26:12 +0000

    Added integration with RayLib

--- src/lraspi.c
+++ src/lraspi.c
@@ -1,12 +1,22 @@
+#include <stdio.h>
+
 #include "lua.h"
 #include "lauxlib.h"
+#include "lraspi.h"
 
 int main(int argc, char **argv)
 {
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
+
+    lraspi_init();
+
     luaL_dofile(L, "main.lua");
     lua_close(L);
+
+    lraspi_close();
+
+    printf("End\n");
 
     return 0;
 }
